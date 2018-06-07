a4a0bbdc09747b89f091ffb6f6323b6ac7dd7371
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-06-07 18:01:49 +0900

    token.list

--- app/coupon/views.py
+++ app/coupon/views.py
@@ -185,7 +185,7 @@ def transfer():
                     transfer(token.token_address, to_address, amount).\
                     transact({'from':owner, 'gas':transfer_gas})
             flash('処理を受け付けました。割当完了までに数分程かかることがあります。', 'success')
-            return redirect(url_for('token.tokenlist'))
+            return redirect(url_for('token.list'))
         else:
             flash_errors(form)
             return render_template('coupon/transfer.html', form=form)
