e16cffef991d25c3e3c1eb491bfec49935466405
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-06-20 18:58:54 +0900

    str

--- create_rsakey.py
+++ create_rsakey.py
@@ -18,7 +18,7 @@ def create(passphrase):
     with open('public.pem', 'wb') as f:
         f.write(public_pem)
 
-    print(public_pem)
+    print(str(public_pem))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="秘密鍵のパスワードを入力してください。")
