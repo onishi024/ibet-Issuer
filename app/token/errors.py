b5843975f25a6ac7c8045781cd2cb371c63e78a4
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-11-06 12:02:18 +0900

    exception

--- app/token/errors.py
+++ app/token/errors.py
@@ -28,3 +28,11 @@ def internal_server_error(e):
         return response
     return render_template('500.html'), 500
 
+@token.errorhandler(Exception)
+def error_handler(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
+    return render_template('500.html'), 500
