9263cc6b2fd84890eb5671a5cf95dd29e9718a66
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-11-26 23:28:17 +0900

    トークン・トークンの保有者のバッチ登録処理

---
+++ script/INSERT_token.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import base64
+import argparse
+import time
+import sys
+
+path = os.path.join(os.path.dirname(__file__), "../")
+sys.path.append(path)
+
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from eth_utils import to_checksum_address
+from app.contracts import Contract
+
+WEB3_HTTP_PROVIDER = os.environ.get('WEB3_HTTP_PROVIDER') or 'http://localhost:8545'
+web3 = Web3(Web3.HTTPProvider(WEB3_HTTP_PROVIDER))
+web3.middleware_stack.inject(geth_poa_middleware, layer=0)
+
+ETH_ACCOUNT = os.environ.get('ETH_ACCOUNT') or web3.eth.accounts[0]
+ETH_ACCOUNT = to_checksum_address(ETH_ACCOUNT)
+ETH_ACCOUNT_PASSWORD = os.environ.get('ETH_ACCOUNT_PASSWORD')
+TOKEN_LIST_CONTRACT_ADDRESS = to_checksum_address(os.environ.get('TOKEN_LIST_CONTRACT_ADDRESS'))
+IBET_SB_EXCHANGE_CONTRACT_ADDRESS = \
+    to_checksum_address(os.environ.get('IBET_SB_EXCHANGE_CONTRACT_ADDRESS'))
+IBET_MEMBERSHIP_EXCHANGE_CONTRACT_ADDRESS = \
+    to_checksum_address(os.environ.get('IBET_MEMBERSHIP_EXCHANGE_CONTRACT_ADDRESS'))
+IBET_CP_EXCHANGE_CONTRACT_ADDRESS = \
+    to_checksum_address(os.environ.get('IBET_CP_EXCHANGE_CONTRACT_ADDRESS'))
+
+# トークン発行
+def issue_token(exchange_address, data_count, token_type):
+    attribute = {}
+    attribute['name'] = 'SEINO_TEST_TOKEN'
+    attribute['symbol'] = 'SEINO'
+    attribute['totalSupply'] = data_count
+    attribute['tradableExchange'] = exchange_address
+    attribute['memo'] = 'memo'
+    attribute['details'] = 'details'
+    attribute['expirationDate'] = '20181010'
+    attribute['transferable'] = True
+    if token_type == 'IbetStraightBond':
+        attribute['faceValue'] = 100
+        attribute['interestRate'] = 1
+        attribute['interestPaymentDate1'] = '20181010'
+        attribute['redemptionDate'] = '20181010'
+        attribute['redemptionAmount'] = 100
+        attribute['returnDate'] = '20181010'
+        attribute['returnAmount'] = 'returnAmount'
+        attribute['purpose'] = 'purpose'
+        interestPaymentDate = json.dumps({
+            'interestPaymentDate1':attribute['interestPaymentDate1']
+        })
+        arguments = [
+            attribute['name'], attribute['symbol'], attribute['totalSupply'],
+            attribute['tradableExchange'],
+            attribute['faceValue'], attribute['interestRate'], interestPaymentDate,
+            attribute['redemptionDate'], attribute['redemptionAmount'],
+            attribute['returnDate'], attribute['returnAmount'],
+            attribute['purpose'], attribute['memo']
+        ]
+    elif token_type == 'IbetMembership':
+        attribute['returnDetails'] = 'returnDetails'
+        arguments = [
+            attribute['name'], attribute['symbol'], attribute['totalSupply'],
+            attribute['tradableExchange'],
+            attribute['details'], attribute['returnDetails'],
+            attribute['expirationDate'], attribute['memo'],
+            attribute['transferable']
+        ]
+    elif token_type == 'IbetCoupon':
+        arguments = [
+            attribute['name'], attribute['symbol'], attribute['totalSupply'],
+            attribute['tradableExchange'],
+            attribute['details'],  attribute['memo'], attribute['expirationDate'],
+            attribute['transferable']
+        ]
+
+    web3.eth.defaultAccount = ETH_ACCOUNT
+    web3.personal.unlockAccount(ETH_ACCOUNT, ETH_ACCOUNT_PASSWORD)
+    contract_address, abi = Contract.deploy_contract(token_type, arguments, ETH_ACCOUNT)
+
+    return {'address': contract_address, 'abi': abi}
+
+# トークンリスト登録
+def register_token_list(token_dict, token_type):
+    TokenListContract = Contract.get_contract('TokenList', TOKEN_LIST_CONTRACT_ADDRESS)
+    web3.eth.defaultAccount = ETH_ACCOUNT
+    web3.personal.unlockAccount(ETH_ACCOUNT, ETH_ACCOUNT_PASSWORD)
+    tx_hash = TokenListContract.functions.register(token_dict['address'], token_type).\
+        transact({'from':ETH_ACCOUNT, 'gas':4000000})
+    tx = web3.eth.waitForTransactionReceipt(tx_hash)
+    print("TokenListContract Length:" + str(TokenListContract.functions.getListLength().call()))
+
+
+def main(data_count, token_type, status):
+    web3.personal.unlockAccount(ETH_ACCOUNT, ETH_ACCOUNT_PASSWORD, 10000)
+    if token_type == 'IbetStraightBond':
+        exchange_address = IBET_SB_EXCHANGE_CONTRACT_ADDRESS
+    elif token_type == 'IbetMembership':
+        exchange_address = IBET_MEMBERSHIP_EXCHANGE_CONTRACT_ADDRESS
+    elif token_type == 'IbetCoupon':
+        exchange_address = IBET_CP_EXCHANGE_CONTRACT_ADDRESS
+    print("exchange_address: " + exchange_address)
+    # token登録
+    for count in range(0, data_count):
+        # トークン発行
+        token_dict = issue_token(exchange_address, data_count, token_type)
+        # トークンリストに登録
+        register_token_list(token_dict, token_type)
+        print("count: " + str(count))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="トークンの登録")
+    parser.add_argument("data_count", type=int, help="登録件数")
+    parser.add_argument("token_type", type=str, help="IbetStraightBond, IbetMembership, IbetCoupon")
+    args = parser.parse_args()
+
+    if not args.data_count:
+        raise Exception("登録件数が必要")
+
+    if not args.token_type:
+        raise Exception("IbetStraightBond, IbetMembership, IbetCoupon")
+
+    main(args.data_count, args.token_type)
