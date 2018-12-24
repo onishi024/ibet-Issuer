a2d51ada064a5f689d5000405b772de5fb548a13
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-12-25 02:25:45 +0900

    リファクタリング＆テストコード追加

--- app/tests/test_account.py
+++ app/tests/test_account.py
@@ -7,10 +7,394 @@ from .conftest import TestBase
 from .account_config import eth_account
 from config import Config
 from .contract_modules import *
+from ..models import User
 
 from logging import getLogger
 logger = getLogger('api')
 
+# 初期設定ユーザ
+# [
+#      {
+#         'login_id': 'admin',
+#         'user_name': '管理者',
+#         'role_id': 1,
+#         'password': '1234'
+#     },{
+#         'login_id': 'user',
+#         'user_name': 'ユーザ',
+#         'role_id': 2,
+#         'password': '1234'
+#     },
+# ]
+
+# ユーザ一覧
+class TestAccountList(TestBase):
+
+    target_url = '/account/list'
+    target_url_404 = '/account/listtttt'
+
+    # ＜正常系１＞
+    # 通常参照（管理者ロール）
+    def test_normal_1(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.get(self.target_url)
+        assert response.status_code == 200
+        assert '<title>アカウント一覧'.encode('utf-8') in response.data
+        assert '<td><a href="/account/edit/1">admin</a></td>'.encode('utf-8') in response.data
+        assert '<td><a href="/account/edit/2">user</a></td>'.encode('utf-8') in response.data
+
+    # ＜エラー系１＞
+    # 権限なしエラー
+    def test_error_1(self, app):
+        client = self.client_with_user_login(app)
+        response = client.get(self.target_url)
+        assert response.status_code == 403
+
+    # ＜エラー系２＞
+    # 権限なしエラー
+    def test_error_2(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.get(self.target_url_404)
+        assert response.status_code == 404
+
+# ユーザ登録
+class TestAccountRegist(TestBase):
+
+    target_url = '/account/regist'
+
+    # ＜正常系１＞
+    # 初期表示：GET
+    def test_normal_1(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.get(self.target_url)
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+
+    # ＜正常系2＞
+    # 登録：POST
+    def test_normal_2(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'test_normal',
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':1,
+            }
+        )
+        assert response.status_code == 302
+
+    # ＜エラー系1-1＞
+    # 権限エラー：管理者ロール以外での参照（GET）
+    def test_error_1_1(self, app):
+        client = self.client_with_user_login(app)
+        response = client.get('/account/regist')
+        assert response.status_code == 403
+
+    # ＜エラー系1-2＞
+    # 権限エラー：管理者ロール以外での参照（POST）
+    def test_error_1_2(self, app):
+        client = self.client_with_user_login(app)
+        response = client.post('/account/regist')
+        assert response.status_code == 403
+
+    # ＜エラー系2-1＞
+    # 登録（POST）：入力値なし（全て）
+    def test_error_2(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post('/account/regist')
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+
+    # ＜エラー系2-2＞
+    # 登録（POST）：入力値なし（login_id）
+    def test_error_2_2(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            '/account/regist',
+            data={
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':1,
+            }
+        )
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+        assert 'ログインIDは必須です。'.encode('utf-8') in response.data
+
+    # ＜エラー系2-3＞
+    # 登録（POST）：入力値なし（user_name）
+    def test_error_2_3(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'test_user',
+                'icon':'',
+                'role':1,
+            }
+        )
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+        assert 'ユーザー名は必須です。'.encode('utf-8') in response.data
+
+    # ＜エラー系3-1＞
+    # 登録（POST）：入力誤り
+    # login_idが短い（３文字以下）
+    def test_error_3_1(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'abc',
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':1,
+            }
+        )
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+        assert 'ログインIDは4文字以上12文字までです。'.encode('utf-8') in response.data
+
+    # ＜エラー系3-2＞
+    # 登録（POST）：入力誤り
+    # login_idに半角英数アンダースコア以外の文字が含まれる
+    def test_error_3_2(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'abcd?',
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':1,
+            }
+        )
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+        assert 'ログインIDは半角英数アンダースコアのみ使用可能です。'.encode('utf-8') in response.data
+
+    # ＜エラー系4＞
+    # 登録（POST）：データ重複エラー
+    # 既に登録されているlogin_idを追加登録
+    def test_error_4(self, app):
+        client = self.client_with_admin_login(app)
+
+        # 1回目
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'test_user',
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':1,
+            }
+        )
+
+        # 2回目
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'test_user',
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':1,
+            }
+        )
+
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+        assert 'このログインIDは既に使用されています。'.encode('utf-8') in response.data
+
+    # ＜エラー系5＞
+    # 登録（POST）：存在しないロールを指定
+    def test_error_5(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            '/account/regist',
+            data={
+                'login_id':'user_error',
+                'user_name':'テストユーザ',
+                'icon':'',
+                'role':10,
+            }
+        )
+
+        assert response.status_code == 200
+        assert '<title>アカウント追加'.encode('utf-8') in response.data
+        assert 'Not a valid choice'.encode('utf-8') in response.data
+
+# ユーザ更新（管理者）
+class TestAccountEdit(TestBase):
+
+    target_url = '/account/edit/'
+
+    # ＜正常系１＞
+    # 初期表示：GET
+    def test_normal_1(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.get(self.target_url + '1')
+        assert response.status_code == 200
+        assert '<title>アカウント編集'.encode('utf-8') in response.data
+
+    # ＜正常系2＞
+    # 登録：POST
+    def test_normal_2(self, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            self.target_url + '1',
+            data={
+                'login_id':'admin',
+                'user_name':'管理者',
+                'icon':'',
+                'role':1,
+            }
+        )
+        assert response.status_code == 302
+
+# ユーザ更新
+class TestAccountEditCurrent(TestBase):
+
+    target_url = '/account/edit_current'
+
+    # ＜正常系１＞
+    # 初期表示：GET
+    def test_normal_1(self, app):
+        client = self.client_with_user_login(app)
+        response = client.get(self.target_url)
+        assert response.status_code == 200
+        assert '<title>アカウント編集'.encode('utf-8') in response.data
+
+    # ＜正常系2＞
+    # 登録：POST
+    def test_normal_2(self, app):
+        client = self.client_with_user_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'login_id':'user',
+                'user_name':'ユーザ',
+                'icon':''
+            }
+        )
+        assert response.status_code == 302
+
+# パスワード変更
+class TestAccountPwdChg(TestBase):
+
+    target_url = '/account/pwdchg'
+
+    # ＜正常系１＞
+    # GET
+    def test_normal_1(self, app):
+        client = self.client_with_user_login(app)
+        response = client.get(self.target_url)
+        assert response.status_code == 200
+        assert '<title>パスワード変更'.encode('utf-8') in response.data
+
+    # ＜正常系2＞
+    # POST
+    def test_normal_2(self, app):
+        client = self.client_with_user_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'password':'1234',
+                'confirm':'1234'
+            }
+        )
+        assert response.status_code == 302
+
+    # ＜エラー系1＞
+    # パスワードなし
+    def test_error_1(self, app):
+        client = self.client_with_user_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'confirm':'1234'
+            }
+        )
+        assert response.status_code == 200
+        assert '<title>パスワード変更'.encode('utf-8') in response.data
+        assert '新しいパスワードが入力されていません。'.encode('utf-8') in response.data
+
+    # ＜エラー系2＞
+    # 確認用パスワードが一致しない
+    def test_error_2(self, app):
+        client = self.client_with_user_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'password':'1234',
+                'confirm':'4321'
+            }
+        )
+        assert response.status_code == 200
+        assert '<title>パスワード変更'.encode('utf-8') in response.data
+        assert 'パスワードが一致しません。'.encode('utf-8') in response.data
+
+# パスワード初期化
+class TestAccountPwdInit(TestBase):
+
+    target_url = '/account/pwdinit'
+
+    # ＜正常系1＞
+    # POST
+    def test_normal_1(self, db, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'id':'2'
+            }
+        )
+        assert response.status_code == 302
+
+        # DB戻し
+        user = db.session.query(User).filter(User.id==2).first()
+        user.password = '1234'
+
+# ユーザ削除
+class TestAccountDelete(TestBase):
+
+    target_url = '/account/delete'
+
+    # ＜正常系1＞
+    # POST
+    def test_normal_1(self, db, app):
+        client = self.client_with_admin_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'login_id':'user'
+            }
+        )
+        assert response.status_code == 302
+        db.session.commit()
+
+        # DB戻し
+        user = User()
+        user.login_id = 'user'
+        user.user_name = 'ユーザ'
+        user.role_id = 2
+        user.password = '1234'
+        db.session.add(user)
+
+    # ＜エラー系1＞
+    # 権限エラー
+    def test_error_1(self, db, app):
+        client = self.client_with_user_login(app)
+        response = client.post(
+            self.target_url,
+            data={
+                'login_id':'user'
+            }
+        )
+        assert response.status_code == 403
+
+# 銀行情報登録
 class TestBankInfo(TestBase):
     url_bankinfo = '/account/bankinfo'
 
@@ -38,7 +422,6 @@ class TestBankInfo(TestBase):
         assert '<input class="form-control" id="account_number" name="account_number" type="text" value="">'.encode('utf-8') in response.data
         assert '<input class="form-control" id="account_holder" name="account_holder" type="text" value="">'.encode('utf-8') in response.data
 
-    '''
     # ＜正常系２＞
     # 登録　→　正常参照
     def test_normal_2(self, app, shared_contract):
@@ -157,7 +540,6 @@ class TestBankInfo(TestBase):
         assert whitelist_json['bank_account']['account_type'] == 4
         assert whitelist_json['bank_account']['account_number'] == '7654321'
         assert whitelist_json['bank_account']['account_holder'] == 'ﾃｽﾄ'
-    '''
 
     # ＜エラー系1-1＞
     # 必須系
