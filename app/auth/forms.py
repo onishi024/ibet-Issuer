86f2a5b1fc60129a34c0416792edb1b460f3f6c6
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-06-17 14:56:53 +0900

    required -> datarequired

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    login_id = StringField("LoginId", validators=[Required("ログインIDを入力してください。")])
-    password = PasswordField("Password", validators=[Required("パスワードを入力してください。")])
+    login_id = StringField("LoginId", validators=[DataRequired("ログインIDを入力してください。")])
+    password = PasswordField("Password", validators=[DataRequired("パスワードを入力してください。")])
 
     class Meta:
         csrf = False
