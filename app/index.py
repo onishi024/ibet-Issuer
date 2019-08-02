5e83117ba16a6869b83cad10e5edd6bcd5b45792
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-08-02 15:27:26 +0900

    fix

--- app/index.py
+++ app/index.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required
 
 from logging import getLogger
@@ -10,4 +10,4 @@ index_blueprint = Blueprint('index', __name__)
 @index_blueprint.route('/', methods=['GET'])
 @login_required
 def index():
-    return render_template('main.html')
+    return redirect(url_for('dashboard.main'))
