8196fdae146775b6d930653b11b1419d7ad40d77
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-08-14 15:25:18 +0900

    refactoring

--- async/processor_BatchTransfer_coupon.py
+++ async/processor_BatchTransfer_coupon.py
@@ -9,6 +9,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 from eth_utils import to_checksum_address
 
 path = os.path.join(os.path.dirname(__file__), '../')
@@ -23,80 +24,78 @@ from config import Config
 log_fmt = 'PROCESSOR [%(asctime)s] [%(process)d] [%(levelname)s] %(message)s'
 logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-"""
-CSVアップロードされた割当情報から、Transferコントラクトを実行する
-"""
-# 設定情報の読み込み
+# 設定情報の取得
 WEB3_HTTP_PROVIDER = os.environ.get('WEB3_HTTP_PROVIDER') or 'http://localhost:8545'
+URI = os.environ.get('DATABASE_URL') or 'postgresql://issueruser:issuerpass@localhost:5432/issuerdb'
+exchange_address = Config.IBET_COUPON_EXCHANGE_CONTRACT_ADDRESS
+
+# 初期化
 web3 = Web3(Web3.HTTPProvider(WEB3_HTTP_PROVIDER))
-URI = os.environ.get('DATABASE_URL') or 'postgresql://bsuser:bspass@localhost:5432/bsdb'
+web3.middleware_stack.inject(geth_poa_middleware, layer=0)
 engine = create_engine(URI, echo=False)
 db_session = scoped_session(sessionmaker())
 db_session.configure(bind=engine)
 
-token_exchange_address = Config.IBET_COUPON_EXCHANGE_CONTRACT_ADDRESS
+# Exchangeコントラクトへの接続
 
-####################################################
-# Transfer Contract
-####################################################
-def transfer_token(TokenContract, from_address, to_address, amount):
-    token_exchange_address = Config.IBET_COUPON_EXCHANGE_CONTRACT_ADDRESS
-    ExchangeContract = Contract.get_contract(
-        'IbetCouponExchange', token_exchange_address)
-
-    # 取引所コントラクトへトークン送信
-    deposit_gas = TokenContract.estimateGas(). \
-        transferFrom(from_address, token_exchange_address, amount)
-    TokenContract.functions. \
-        transferFrom(from_address, token_exchange_address, amount). \
-        transact({'from': Config.ETH_ACCOUNT, 'gas': deposit_gas})
-
-    # 取引所コントラクトからtransferで送信相手へ送信
-    transfer_gas = ExchangeContract.estimateGas(). \
-        transfer(to_checksum_address(TokenContract.address), to_address, amount)
-    tx_hash = ExchangeContract.functions. \
-        transfer(to_checksum_address(TokenContract.address), to_address, amount). \
-        transact({'from': Config.ETH_ACCOUNT, 'gas': transfer_gas})
-    return tx_hash
-
+exchange_contract = Contract.get_contract('IbetCouponExchange', exchange_address)
 
 # 常時起動（無限ループ）
 while True:
-    logging.info('start batch')
-    # Issue済ではない割当一覧を抽出
+    logging.info('[CouponBatchTransfer] Loop Start')
+
+    # 割当（Transfer）済ではないレコードを抽出
     try:
-        untransferred_list = db_session.query(CouponBulkTransfer). \
+        untransfered_list = db_session.query(CouponBulkTransfer). \
             filter(CouponBulkTransfer.transferred == False).all()
     except Exception as err:
         logging.error("%s", err)
         break
 
-    # EOAのアンロック
-    eth_unlock_account()
+    # レコード単位で割当処理を実行
+    for item in untransfered_list:
 
-    for item in untransferred_list:
-        # Tokenコントラクト接続
-        token = db_session.query(Token).filter(Token.token_address == item.coupon_address).first()
+        # Tokenコントラクトへの接続
+        token = db_session.query(Token).filter(Token.token_address == item.token_address).first()
         token_abi = json.loads(token.abi.replace("'", '"').replace('True', 'true').replace('False', 'false'))
         TokenContract = web3.eth.contract(
             address=token.token_address,
             abi=token_abi
         )
-        # 割当処理（発行体アドレス→指定アドレス）
-        from_address = Config.ETH_ACCOUNT
+
+        # 割当処理
+        from_address = Config.ETH_ACCOUNT # 発行体アドレス
         to_address = to_checksum_address(item.to_address)
         amount = item.amount
-        tx_hash = transfer_token(TokenContract, from_address, to_address, amount)
 
-        # 発行済状態に更新
         try:
+            # EOAのアンロック
+            eth_unlock_account()
+
+            # 取引所コントラクトへデポジット（Transfer）
+            deposit_gas = TokenContract.estimateGas(). \
+                transferFrom(from_address, exchange_address, amount)
+            TokenContract.functions. \
+                transferFrom(from_address, exchange_address, amount). \
+                transact({'from': Config.ETH_ACCOUNT, 'gas': deposit_gas})
+
+            # 取引所コントラクトから割当先へTransfer
+            transfer_gas = exchange_contract.estimateGas(). \
+                transfer(to_checksum_address(TokenContract.address), to_address, amount)
+            exchange_contract.functions. \
+                transfer(to_checksum_address(TokenContract.address), to_address, amount). \
+                transact({'from': Config.ETH_ACCOUNT, 'gas': transfer_gas})
+
+            # DBを割当済に更新
             item.transferred = True
-            logging.info("The coupon was transferred. : {}".format(item.coupon_address))
+
+            logging.info("[CouponBatchTransfer] Success : {}".format(item.token_address))
+
         except Exception as err:
             logging.error("%s", err)
             break
-        logging.info('db updated')
 
         db_session.commit()
 
+    logging.info('[CouponBatchTransfer] Loop Finished')
     time.sleep(3)
