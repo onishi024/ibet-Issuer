7184bac7b2b3539b5310a41866ed24fd56102de5
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-08-29 15:18:36 +0900

    remove nohup

--- run_market_make.sh
+++ run_market_make.sh
@@ -4,5 +4,5 @@ source ~/.bash_profile
 cd /app/tmr-issuer
 
 # script
-nohup python script/swap_market_make_buy.py < /dev/null 2>&1 /dev/null &
-nohup python script/swap_market_make_sell.py < /dev/null 2>&1 /dev/null &
+python script/swap_market_make_buy.py < /dev/null 2>&1 /dev/null &
+python script/swap_market_make_sell.py < /dev/null 2>&1 /dev/null &
