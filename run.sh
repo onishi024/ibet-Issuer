016025d2bef56e4b9991104cec3b532b7aedb04c
YoshihitoAso <y-aso@boostry.co.jp>
2019-12-18 21:04:13 +0900

    feat: deactivate mrf processor

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ cd /app/ibet-Issuer
 # async
 nohup python async/processor_IssueEvent.py < /dev/null 2>&1 /dev/null &
 nohup python async/processor_BatchTransfer_coupon.py < /dev/null 2>&1 /dev/null &
-nohup python async/processor_BatchTransfer_mrf.py < /dev/null 2>&1 /dev/null &
 
 #run server
 gunicorn -b 0.0.0.0:5000 --reload manage:app --config guniconf.py
