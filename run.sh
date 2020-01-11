66434d746028dd98632d52fe9c0ee6f75c26b49e
YoshihitoAso <y-aso@boostry.co.jp>
2020-01-12 01:42:33 +0900

    fix: standard output

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ source ~/.bash_profile
 cd /app/ibet-Issuer
 
 # async
-nohup python async/processor_IssueEvent.py < /dev/null 2>&1 /dev/null &
-nohup python async/processor_BatchTransfer_coupon.py < /dev/null 2>&1 /dev/null &
-nohup python async/processor_Order.py < /dev/null 2>&1 /dev/null &
+python async/processor_IssueEvent.py &
+python async/processor_BatchTransfer_coupon.py &
+python async/processor_Order.py &
 
 #run server
 gunicorn -b 0.0.0.0:5000 --reload manage:app --config guniconf.py
