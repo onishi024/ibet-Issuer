2d27f33291f1283bb9540ab160ab41fbd3ee35d2
YoshihitoAso <y-aso@boostry.co.jp>
2020-03-09 17:55:21 +0900

    fix: fix build error

--- app/tests/qa.sh
+++ app/tests/qa.sh
@@ -3,5 +3,8 @@ source ~/.bash_profile
 
 cd /app/ibet-Issuer
 
+mv ./app/tests/data/rsa/test_private.pem ./data/rsa/private.pem
+mv ./app/tests/data/rsa/test_public.pem ./data/rsa/public.pem
+
 # test
 python manage.py test -v --cov 
